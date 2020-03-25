940b5425e587737f6b1d2f397a607fc9e79c69b2
Karina Machado <karinamachado@MacBook-Pro-de-Karina.local>
2020-03-25 16:04:01 -0300

    sidebar fixed

--- src/components/04-modules/sidebar-blog/sidebar-blog.js
+++ src/components/04-modules/sidebar-blog/sidebar-blog.js
@@ -5,10 +5,10 @@ var header = document.getElementById("sidebar");
 var scroll = header.offsetTop;
 
 function sideBar() {
-  if (window.pageYOffset > scroll) {
-    header.classList.add("scroll");
+  if (window.pageYOffset >= scroll ) {
+    header.classList.add("sidebar-section");
   } else {
-    header.classList.remove("scroll");
+    header.classList.remove("sidebar-section");
   }
 }
 
