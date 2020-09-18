3f13480ab627cd5f18f60f3df5700689d4408916
Douglas Cantão <dcp.pereira@gmail.com>
2020-09-17 21:54:16 -0300

    Adding validation to not duplicate span

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,14 +1,13 @@
 window.onload = (event) => {
     const links = document.body.querySelectorAll('a[target="_blank"]')
 
-    links.forEach( tag => {          
-
+    links.forEach( aTag => {          
         let spanTag = document.createElement('span');
         spanTag.className = 'screen-reader-only';
         spanTag.innerHTML = '(opens in a new tab)';
 
-        if(tag.children.childElementCount < 1){
-            tag.appendChild(spanTag)
+        if (aTag.querySelectorAll('span.screen-reader-only').length === 0) {
+            aTag.appendChild(spanTag)
         }
     })
 };
